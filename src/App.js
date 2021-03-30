979513607d8a9881365bfadbfde292c36142bd8c
Adilson Ramos <75184157+AdilsonRamosO@users.noreply.github.com>
2021-03-29 22:14:01 -0300

    Versão do History

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import React from 'react';
+import React from 'react'
 import { ToastContainer } from 'react-toastify'
 import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux'
-import { Router, BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom'
 
-import './config/ReactotronConfig';
+import './config/ReactotronConfig'
 
-import Routes from './routes';
-import history from './services/history';
+import Routes from './routes'
+import history from './services/history'
 
 import { store, persistor } from './store'
 
-import GlobalStyle from './styles/global';
+import GlobalStyle from './styles/global'
 
 function App() {
   return (
-    <BrowserRouter>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <Router history={history}>
-          <Routes />
-          <GlobalStyle />
-          <ToastContainer autoClose={3000}/>
-        </Router>
-      </PersistGate>
-    </Provider>
-    </BrowserRouter>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Router history={history}>
+            <Routes />
+            <GlobalStyle />
+            <ToastContainer autoClose={3000} />
+          </Router>
+        </PersistGate>
+      </Provider>
   )
 }
 
-export default App;
+export default App
