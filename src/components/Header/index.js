2acc91f2e0e0343b3b347d290e97f960f1a79155
Adilson Ramos <75184157+AdilsonRamosO@users.noreply.github.com>
2021-04-03 15:25:16 -0300

    Atualizando perfil/foto de perfil

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import Notifications from '~/components/Notifications'
@@ -8,6 +9,7 @@ import logo from '~/assets/logo-purple.svg'
 import { Container, Content, Profile } from './styles'
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile)
   return (
     <Container>
       <Content>
@@ -20,11 +22,11 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Diego Fernandes</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
-              src="https://avatars.githubusercontent.com/u/37906413?s=400&v=4"
+              src={profile.avatar.url || "https://avatars.githubusercontent.com/u/37906413?s=400&v=4"}
               alt="Diego Fernandes"
             />
           </Profile>
